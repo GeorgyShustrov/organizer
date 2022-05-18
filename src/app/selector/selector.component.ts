ff6e67b12b95ede588bf8356af51c74c75006329
GeorgyShustrov <gosha-s.1999@yandex.ru>
2022-05-18 17:47:52 +0300

    Add calendar

--- src/app/selector/selector.component.ts
+++ src/app/selector/selector.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { DataService } from '../shared/date.service';
 
 @Component({
   selector: 'app-selector',
   templateUrl: './selector.component.html',
-  styleUrls: ['./selector.component.scss']
+  styleUrls: ['./selector.component.scss'],
 })
-export class SelectorComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class SelectorComponent {
+  constructor(public dateService: DataService) {}
+  go(dir: number) {
+    this.dateService.changeMonth(dir);
   }
-
 }
