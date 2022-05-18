cf8335d5b1acf1f1ad531fa776046cfa386cb07b
GeorgyShustrov <gosha-s.1999@yandex.ru>
2022-05-18 22:01:00 +0300

    Stopped on FireBase

--- src/app/organizer/organizer.component.ts
+++ src/app/organizer/organizer.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { DataService } from '../shared/date.service';
 
 @Component({
   selector: 'app-organizer',
   templateUrl: './organizer.component.html',
-  styleUrls: ['./organizer.component.scss']
+  styleUrls: ['./organizer.component.scss'],
 })
 export class OrganizerComponent implements OnInit {
-
-  constructor() { }
+  form!: FormGroup;
+  constructor(public dateService: DataService) {}
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl('', Validators.required),
+      description: new FormControl(''),
+    });
   }
-
+  submit() {
+    const { title, description } = this.form.value;
+    console.log(title, description);
+  }
 }
